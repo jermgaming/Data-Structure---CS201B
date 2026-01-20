6c6cd9c11a73d3905acf9e0ff2c5ffea8242a2cf
jermgaming <atinder999@gmail.com>
2026-01-20 18:18:46 +0530

    1.7.1

---
+++ Unit-1 - Introduction/1.7 Essential Programs/1.7.1. Sequence Generation.c
@@ -0,0 +1,22 @@
+//Write the code here
+#include<stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	int narr[n];
+	narr[n-1]=arr[n-1];
+	for(int i=n-2;i>=0;i--){
+		//printf("%d %d\n",arr[i],arr[i+1]);
+		narr[i]=arr[i+1]+arr[i];
+	}
+
+	for(int i=0;i<n;i++){
+		printf("%d ",narr[i]);
+	}
+	
+	return 0;
+}
