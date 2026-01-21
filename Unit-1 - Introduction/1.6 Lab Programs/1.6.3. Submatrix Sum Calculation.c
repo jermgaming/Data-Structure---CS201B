0f479d2e97c2d26ab174df4c970ea9ae07121894
jermgaming <atinder999@gmail.com>
2026-01-22 04:49:38 +0530

    1.6.3

---
+++ Unit-1 - Introduction/1.6 Lab Programs/1.6.3. Submatrix Sum Calculation.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+	int n,m;
+	scanf("%d %d",&n,&m);
+	int arr[n][m];
+	for(int i=0;i<n;i++){
+		for(int j=0;j<m;j++){
+			scanf("%d",&arr[i][j]);
+		}
+	}
+	int x1,y1,x2,y2;
+	int ans=0;
+	scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+	for(int i=x1-1;i<x2;i++){
+		for(int j=y1-1;j<y2;j++){
+			ans+=arr[i][j];
+		}
+	}
+	printf("%d\n",ans);
+	return 0;
+}
