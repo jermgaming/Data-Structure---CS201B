27a2ca677f0dd26a431e6f15c4a485870cde0add
jermgaming <atinder999@gmail.com>
2026-01-21 14:02:58 +0530

    1.7.5

---
+++ Unit-1 - Introduction/1.7 Essential Programs/1.7.5. Sort the scores of athletes in descending order.c
@@ -0,0 +1,39 @@
+// write your code here
+#include<stdio.h>
+
+void bubbleSort(int arr[],int n){
+	for(int i=0;i<n;i++){
+		for(int j=0;j<n-i-1;j++){
+			if(arr[j]<arr[j+1]){
+				int temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+}
+void printArr(int arr[],int n){
+	for(int i=0;i<n;i++){
+		printf("%d ",arr[i]);
+	}
+	printf("\n");
+}
+int main(){
+	int n;
+	scanf("%d",&n);
+	if(n<=0){
+		printf("Invalid number\n");
+		return 0;
+	}
+	int arr[n];
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+
+	printf("Original Scores: ");
+	printArr(arr,n);
+	bubbleSort(arr,n);
+	printf("Sorted Scores: ");
+	printArr(arr,n);
+	return 0;
+}
