337b13f4ad4209da0b8f76c3f144bcbb75077372
jermgaming <atinder999@gmail.com>
2026-01-26 17:47:16 +0530

    1.2.1

---
+++ Unit-1 - Introduction/1.2 Lesson 2 - Array/1.2.1. Introduction to Arrays.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+	int arr[10];
+	arr[0] = 10;
+	arr[1] = 20;
+	arr[2] = 100;
+	arr[3] = 200;
+	printf("The value in arr[0] : %d\n",arr[0]  ); // Print the 0th element of arr
+	printf("The value in arr[1] : %d\n", arr[1] ); // Print the 1st element of arr
+	printf("The value in arr[2] : %d\n",arr[2]  ); // Print the 2nd element of arr
+	printf("The value in arr[3] : %d\n", arr[3] ); // Print the 3rd element of arr
+}
