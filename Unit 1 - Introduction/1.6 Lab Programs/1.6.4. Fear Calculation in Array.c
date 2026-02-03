24fb79a04c2f19d2f2c9c1d63f534205a4895d3e
jermgaming <atinder999@gmail.com>
2026-02-03 22:08:02 +0530

    1.6.4

---
+++ Unit 1 - Introduction/1.6 Lab Programs/1.6.4. Fear Calculation in Array.c
@@ -0,0 +1,27 @@
+// Type Content here...
+#include<stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	int fear[n];
+	fear[n-1]=1;
+	for(int i=0;i<n-1;i++){
+		for(int j=i+1;j<n;j++){
+			if(arr[i]>arr[j]){
+				fear[i]=j+1-i;
+				break;
+			}
+			fear[i]=1;
+		}
+	}
+	int ans=1;
+	for(int i=0;i<n;i++){
+		ans=ans*fear[i];
+	}
+	printf("%d\n",ans);
+	return 0;
+}
