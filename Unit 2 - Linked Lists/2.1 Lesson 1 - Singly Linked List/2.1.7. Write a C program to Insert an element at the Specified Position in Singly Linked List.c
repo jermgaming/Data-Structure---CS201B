cece6105dabe3998bfe12031a7716abb8a2e6954
jermgaming <atinder999@gmail.com>
2026-02-09 09:51:53 +0530

    2.1.7

--- Unit 2 - Linked Lists/2.1 Lesson 1 - Singly Linked List/2.1.7. Write a C program to Insert an element at the Specified Position in Singly Linked List.c
+++ Unit 2 - Linked Lists/2.1 Lesson 1 - Singly Linked List/2.1.7. Write a C program to Insert an element at the Specified Position in Singly Linked List.c
@@ -12,44 +12,25 @@ NODE createNode() {
 }
 
 NODE insertAtPosition(NODE first, int pos, int x) {
-	if(first==NULL && pos!=1){
-		printf("No such position in SLL so insertion is not possible\n");
-		return NULL;
-	}
-	if(first==NULL){
-		if(pos==1){
-			first=createNode();
-			first->data=x;
-			return first;
-		}
-		return first;
-	}
+	
+	// Write your code here
+	NODE node=createNode();
+	node->data=x;
 	if(pos==1){
-			NODE temp=createNode();
-			temp->data=x;
-			temp->next=first;
-			first=temp;
-			return first;
-	}
-	
-	NODE temp=first;
-	NODE prev=NULL;
-	int count=1;
-	while(temp && count <pos){
-		prev=temp;
-		temp=temp->next;
-		count++;
-	}
-	if(temp==NULL && count<pos){
-		printf("No such position in SLL so insertion is not possible\n");
+		node->next=first;
+		first=node;
 		return first;
 	}
-	NODE newNode=createNode();
-	newNode->data=x;
-	newNode->next=temp;
-	prev->next=newNode;
+	NODE temp=first;
+	for(int i=1;i<pos-1;i++){
+		temp=temp->next;
+	}
+	if(!temp){
+		printf("No such position in SLL so insertion is not possible\n");
+	}
+	node->next=temp->next;
+	temp->next=node;
 	return first;
-	
 }
 
 void traverseList(NODE first) {
