4614bdc144f897198b5e82bd0b8631284a6e148a
jermgaming <atinder999@gmail.com>
2026-03-28 16:46:54 +0530

    4.1.3

---
+++ Unit 4 - Queue/4.1 Lesson 1 - Operations of Queue/4.1.3. Implementation of Queue using Arrays - dequeue(), size() and isEmpty().c
@@ -0,0 +1,57 @@
+#define MAX 10
+int queue[MAX];
+int front = -1, rear = -1;
+
+void dequeue() {
+	if(front==-1 || front>rear){
+		printf("Queue is underflow.\n");
+	}else {
+		printf("Deleted element = %d\n",queue[front++]);
+	}
+
+	if(front>rear){
+		front=-1;
+		rear=-1;
+	}
+}
+
+void size() {
+	if(front==-1 || front>rear){
+		printf("Queue size : %d\n",0);
+	}else{
+		printf("Queue size : %d\n",rear-front+1);
+	}
+}
+
+void isEmpty() {
+	if(front==-1 || front>rear){
+		printf("Queue is empty.\n");
+	}else {
+		printf("Queue is not empty.\n");
+	}
+}
+
+void enqueue(int x) {
+	if (rear == MAX - 1) {
+		printf("Queue is overflow.\n");
+	} else {
+		rear++;
+		queue[rear] = x;
+		printf("Successfully inserted.\n");
+	}
+	if (front == -1) {
+		front++;
+	}
+}
+
+void display() {
+	if (front == -1 && rear == -1) {
+		printf("Queue is empty.\n");
+	} else {
+		printf("Elements in the queue : ");
+		for (int i = front; i <= rear; i++) {
+			printf("%d ",queue[i]);
+		}
+		printf("\n");
+	}
+}
