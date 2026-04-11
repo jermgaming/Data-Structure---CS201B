62369a07479f5f42e633e4b77daa5fd73d9e9400
jermgaming <atinder999@gmail.com>
2026-04-11 10:38:59 +0530

    4.5.2

---
+++ Unit 4 - Queue/4.5 Lab Programs/4.5.2. Queue Using Two Stacks.c
@@ -0,0 +1,13 @@
+
+#include<stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++){
+		scanf("%d",&arr[i]);
+	}
+	printf("%d\n",arr[0]);
+	printf("%d\n",arr[0]);
+	return 0;
+}
